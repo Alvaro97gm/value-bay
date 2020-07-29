75e6986d867f4382ad036da35b1e56a584b2a0ae
alvaro97gm <Alvaro97gm@gmail.com>
2020-07-29 22:12:17 +0200

    IntelliJ-Repository integration

---
+++ src/main/java/com/alvarogm/valuebay/ValueBayApplication.java
@@ -0,0 +1,12 @@
+package com.alvarogm.valuebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueBayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alvarogm/valuebay/ValueBayApplicationTests.java
@@ -0,0 +1,8 @@
+package com.alvarogm.valuebay;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueBayApplicationTests {
+
+}
